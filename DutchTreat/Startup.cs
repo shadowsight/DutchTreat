a9cbf29dffd24e70cb28130c46bf35ff2c23e87e
shadowsight <30534190+shadowsight@users.noreply.github.com>
2021-02-28 07:23:01 -0600

    Update 2-28-21

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -19,84 +19,102 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 
 namespace DutchTreat
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
-    }
+        private readonly IConfiguration _config;
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddIdentity<StoreUser, IdentityRole>(cfg =>
-      {
-        cfg.User.RequireUniqueEmail = true;
-      })
-        .AddEntityFrameworkStores<DutchContext>();
-
-      services.AddAuthentication()
-        .AddCookie()
-        .AddJwtBearer(cfg =>
+        public Startup(IConfiguration config)
         {
-          cfg.TokenValidationParameters = new TokenValidationParameters()
-          {
-            ValidIssuer = _config["Tokens:Issuer"],
-            ValidAudience = _config["Tokens:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
-          };
+            _config = config;
+        }
 
-        });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+            })
+              .AddEntityFrameworkStores<DutchContext>();
 
-      services.AddDbContext<DutchContext>(cfg =>
-      {
-        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
-      });
+            services.AddAuthentication()
+              .AddCookie()
+              .AddJwtBearer(cfg =>
+              {
+                  cfg.TokenValidationParameters = new TokenValidationParameters()
+                  {
+                      ValidIssuer = _config["Tokens:Issuer"],
+                      ValidAudience = _config["Tokens:Audience"],
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
+                  };
+              });
 
-      services.AddAutoMapper(Assembly.GetEntryAssembly());
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
 
-      services.AddTransient<DutchSeeder>();
+            services.AddAutoMapper(Assembly.GetEntryAssembly());
 
-      services.AddScoped<IDutchRepository, DutchRepository>();
+            services.AddTransient<DutchSeeder>();
 
-      services.AddTransient<IMailService, NullMailService>();
-      // Support for real mail service
-      
-      services.AddControllersWithViews();
+            services.AddScoped<IDutchRepository, DutchRepository>();
 
+            services.AddTransient<IMailService, NullMailService>();
+            // Support for real mail service
+
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseNodeModules();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                "{controller}/{action}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+
+                /*
+                if (env.IsDevelopment())
+                {
+                    //spa.UseAngularCliServer(npmScript: "start");
+                }
+                */
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-      app.UseNodeModules();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseRouting();
-
-      app.UseEndpoints(cfg =>
-      {
-        cfg.MapControllerRoute("Default",
-              "{controller}/{action}/{id?}",
-              new { controller = "App", Action = "Index" });
-      });
-
-
-    }
-  }
 }
